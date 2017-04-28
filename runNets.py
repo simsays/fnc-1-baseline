5ecab96de381e7563251222b1df908ad35b69c0f
Thomas <tberrueta@g.hmc.edu>
2017-04-28 14:19:11 -0700

    added decay, increased tokenization dimension, changed the merging mode to concat, and added model saving.

--- runNets.py
+++ runNets.py
@@ -2,7 +2,7 @@
 ###########################################
 # Imports
 import numpy as np
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, load_model, model_from_json
 from keras.layers import Dense, Input, Merge
 from keras.layers import LSTM
 from keras.layers import Dropout, Flatten
@@ -36,10 +36,8 @@ MAX_SEQUENCE_LENGTH = int(npzfile['max_seq'])
 # Neural Network structure
 #######################################
 
-
 #######################################
 # Article Branch
-
 # Pre-training embedding layer
 article_branch = Sequential()
 article_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32', name='article_input')
@@ -74,7 +72,6 @@ print(article_branch.summary())
 
 #########################################
 # Headline Branch
-
 headline_branch = Sequential()
 headline_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32', name='headline_input')
 headline_embedding_layer = Embedding(len(headline_word_index.keys()) + 1,
@@ -90,7 +87,7 @@ print(headline_branch.summary())
 ########################################
 # Merged Branch
 model = Sequential()
-model.add(Merge([article_branch, headline_branch], mode = 'ave'))
+model.add(Merge([article_branch, headline_branch], mode = 'concat'))
 
 # LSTM
 lstm = LSTM(20, activation='sigmoid')
@@ -101,16 +98,43 @@ output = Dense(4, activation='softmax')
 model.add(output)
 
 # Optimizer
-sgd = SGD(lr=0.3, momentum=0.8, decay=0, nesterov=False)
+sgd = SGD(lr=0.3, momentum=0.8, decay=.00000005, nesterov=False)
 
 # Model compilation and training
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 print(model.summary())
 print('Training model.')
-model.fit([article_train, headline_train], y_train, epochs=30, batch_size=128)
+model.fit([article_train, headline_train], y_train, epochs=100, batch_size=128)
 
 
 ######################################
 # Final evaluation of the model
+scores = model.evaluate([article_val, headline_val], y_val, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+######################################
+# Saving Model
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
+
+######################################
+# Loading Model
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 scores = model.evaluate([article_val, headline_val], y_val, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
